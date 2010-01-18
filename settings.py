7bec1f57b7cebbeeb8da6b0a13352c4fc5926a02
admin <admin@ab9cdc8a-c177-4a5d-8397-7951b204a726>
2010-01-18 05:24:49 +0000

    had template_dirs mixed up
    git-svn-id: http://svn.albertsun.info/copress/copressmoney@368 ab9cdc8a-c177-4a5d-8397-7951b204a726

--- settings.py
+++ settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'copressmoney.urls'
 
 TEMPLATE_DIRS = (
-    '/Users/albert/stuff/CoPress/copressmoney/copressmoney/templates',
+    '/home/copress/webapps/copressmoney/copressmoney/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
